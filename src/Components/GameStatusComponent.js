c346113163d64e05c2dfa597aa4ec26b8416b490
itzik <itzikd15@gmail.com>
2021-07-22 21:46:22 +0300

    image change

--- src/Components/GameStatusComponent.js
+++ src/Components/GameStatusComponent.js
@@ -5,13 +5,14 @@ import {observer} from "mobx-react";
 
 function GameStatusComponent() {
     const game = useBoard();
+    const imageWidth="200px"
     let content
     if (game.gameStatus === "Win" || game.gameStatus === "Won") {
-        content = <div><img style={{maxWidth: "200px"}} src={"win.png"} alt={"Win"}/></div>
+        content = <div><img style={{maxWidth: imageWidth}} src={"win.png"} alt={"Win"}/></div>
     } else if (game.gameStatus === "Lose" || game.gameStatus === "Lost") {
-        content = <div><img style={{maxWidth: "200px"}} src={"gameover.png"} alt={"Lost"}/></div>
+        content = <div><img style={{maxWidth: imageWidth}} src={"gameOver.png"} alt={"Lost"}/></div>
     } else
-        content = <div><img style={{maxWidth: "200px"}} src={"think.gif"} alt={"Normal"}/></div>
+        content = <div><img style={{maxWidth: imageWidth}} src={"think.gif"} alt={"Normal"}/></div>
 
 
     return (
