563c4f64739dfe9d8153241ea899ad12800a1f96
itzik <itzikd15@gmail.com>
2021-07-23 14:43:03 +0300

    fix deploy

--- src/Components/SettingsComponent.js
+++ src/Components/SettingsComponent.js
@@ -52,7 +52,7 @@ export default function SettingsComponent({height, onChangeHeight, width, onChan
                             <Form.Check
                                 id="switchEnabled"
                                 type="switch"
-                                label="Test Super Mode"
+                                label="Super Mode"
                                 onClick={game.supermanMode}
                             />
                         </Form>
